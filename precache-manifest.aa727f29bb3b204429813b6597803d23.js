c43ba544323ef01ff2c1eaa564fadd690a55effc
openopen <openopen114@gmail.com>
2019-10-04 18:21:34 +0800

    Updates

---
+++ precache-manifest.aa727f29bb3b204429813b6597803d23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b001318516528f1fa597bb341e1672c2",
+    "url": "/hooks-use-state-demo/index.html"
+  },
+  {
+    "revision": "748169a19d9ec94bf048",
+    "url": "/hooks-use-state-demo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f03cb4ab2b0f62de39d7",
+    "url": "/hooks-use-state-demo/static/js/2.bacc89e0.chunk.js"
+  },
+  {
+    "revision": "748169a19d9ec94bf048",
+    "url": "/hooks-use-state-demo/static/js/main.3594dbe4.chunk.js"
+  },
+  {
+    "revision": "7748bd44966420b4d9a3",
+    "url": "/hooks-use-state-demo/static/js/runtime-main.8389413d.js"
+  }
+]);
